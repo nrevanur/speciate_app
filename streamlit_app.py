34c10e0ec71cce906e9be3a10e04a3a63dd3c1e4
Swetha Revanur <swetharevanur@users.noreply.github.com>
2024-12-19 12:59:55 -0500

    Update streamlit_app.py to include query params

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,32 +1,34 @@
-import streamlit as st
-import asyncio, websockets
-import requests
+import asyncio
 import json
 import os
+import websockets
 
-class ChatApplication:
+import requests
+import streamlit as st
+
+class Config:
+    BASE_URL = f"{os.getenv('ENV', 'prod')}.api.speciate.com"
+    CONFIG_URL = f"https://{BASE_URL}/v1/configs/"
+    CHAT_URL = f"wss://{BASE_URL}/v1/chat-agents/chat-message"
+
+    LLM_CONTEXT_LOG_COUNT = 10
+
+    LLM_MODEL_NAME = "claude-3-5-sonnet-20241022"
+
+
+class StreamlitApp:
     def __init__(self):        
-        print()
-        self.user_email = "admin@speciate.com"
-        self.stream_resposnse_flag = True
-
-        domain_suffix = ".api.speciate.com"
-        env_prefix = os.getenv('ENV', "dev")
-        http_api_url = "https://"+env_prefix+domain_suffix ;
-        ws_api_url = "wss://"+env_prefix+domain_suffix ;
+        self.user_email = st.query_params.get("user_email")
+        self.stream_response_flag = True
+        self.connection_url = f"{Config.CHAT_URL}?user_email={self.user_email}&stream_response={self.stream_response_flag}"
         
-        self.config_connect_url=http_api_url+"/v1/configs/"
-        self.chat_connect_url=ws_api_url+"/v1/chat-agents/chat-message"
-        print("config_connect_url:" + self.config_connect_url)
-        print("chat_connect_url:" + self.chat_connect_url)
-
         if "messages" not in st.session_state:
             st.session_state.messages = [
                 {'role': "user", "content": ""},
                 {'role': "assistant", "content": "Understood"},
             ]
 
-    def display_title (self):
+    def display_title(self):
         st.title("Chat with Speciate Agent")
         hide_decoration_bar_style = '''
             <style>
@@ -35,32 +37,28 @@ class ChatApplication:
         '''
         st.markdown(hide_decoration_bar_style, unsafe_allow_html=True)
 
-    def display_config (self):
-        response = requests.get(self.config_connect_url)
-        if (response.ok):
+    def display_config(self):
+        response = requests.get(Config.CONFIG_URL)
+        if response.ok:
             config_parent = json.loads(response.content)
             config = config_parent['config']
             container = st.container(border=True)
-            container.write("_Current Configuration_")
             container.write("Model: " + ":blue["+config['LLM.MODEL_NAME']+"]")
-            container.write("Log Count: :blue[" + str(config['LLM.CONTEXT_LOG_COUNT']) + "]")
-            col1, col2 = container.columns([0.2, 0.8], gap="small", vertical_alignment="center")
-            col1.markdown("Enter Your Email: ")
-            self.user_email = col2.text_input("Enter Your Email: ", value=self.user_email, key="email", max_chars=50, label_visibility="collapsed")
+            if self.user_email:
+                container.write(f"Reading :blue[{str(config['LLM.CONTEXT_LOG_COUNT'])}] most recent logs for :blue[{self.user_email}]")
+            else:
+                container.write("No user context available")
 
-    def display_chat_conversation (self):
+    def display_chat_conversation(self):
         # Display user and assistant messages skipping the first two
         for message in st.session_state.messages[2:]:
             # ignore tool use blocks
             if isinstance(message["content"], str):
                 st.chat_message(message["role"]).markdown(message["content"])
 
-    def get_connection_url (self):
-        return self.chat_connect_url+"?user_email="+self.user_email+"&stream_response="+str(self.stream_resposnse_flag)
-
     async def handle_chat_response(self, chat_request, chat_response_placeholder):
         try:
-            async with websockets.connect(self.get_connection_url()) as websocket:
+            async with websockets.connect(self.connection_url) as websocket:
                 await websocket.send(chat_request)
 
                 full_chat_response = ""
@@ -70,13 +68,13 @@ class ChatApplication:
                         break
                     full_chat_response += chat_response
                     chat_response_placeholder.markdown(full_chat_response)
-                    if self.stream_resposnse_flag == False:
+                    if self.stream_response_flag == False:
                         break
                 return full_chat_response    
         except Exception as e:
             print(str(e))
 
-    async def handle_chat_conversation (self):
+    async def handle_chat_conversation(self):
         if chat_request := st.chat_input("How can I help you today?"):
             st.chat_message("user").markdown("(" + self.user_email + ") " + chat_request)
             st.session_state.messages.append({"role": "user", "content": "(" + self.user_email + ") " + chat_request})
@@ -87,16 +85,11 @@ class ChatApplication:
                     full_chat_response = await self.handle_chat_response(chat_request, chat_response_placeholder)
                     st.session_state.messages.append({"role": "assistant", "content": full_chat_response})
     
-chatApp = ChatApplication()
-
-def main():
-    chatApp.display_title()
-
-    chatApp.display_config()
-
-    chatApp.display_chat_conversation()
-
-    asyncio.run(chatApp.handle_chat_conversation())
 
 if __name__ == "__main__":
-   main()
+    app = StreamlitApp()
+    app.display_title()
+    app.display_config()
+    if app.user_email:
+        app.display_chat_conversation()
+        asyncio.run(app.handle_chat_conversation())
